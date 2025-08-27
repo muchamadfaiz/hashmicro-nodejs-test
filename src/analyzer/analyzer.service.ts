38084c6e46b8db5219b2e3f6305ff6c2c0c33e48
muchamadfaiz <muchamadfaiz@gmail.com>
2025-08-27 15:48:26 +0700

    fix: ignore space

--- src/analyzer/analyzer.service.ts
+++ src/analyzer/analyzer.service.ts
@@ -4,7 +4,9 @@ import { MatchDto } from './dto/match.dto';
 @Injectable()
 export class AnalyzerService {
   checkMatch(dto: MatchDto) {
-    const { input1, input2 } = dto;
+    // const { input1, input2 } = dto;
+    const input1 = dto.input1.replace(/\s/g, '');
+    const input2 = dto.input2.replace(/\s/g, '');
     const lengthInput1 = dto.input1.length;
 
     let matchedCount = 0;
